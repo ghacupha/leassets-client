f736d8e26e241f51135225fdb4200cdf4940167f
Edwin Njeru <mailnjeru@gmail.com>
2021-05-10 16:17:39 +0300

    Template errors and minor fixed

--- src/main/webapp/app/bespoke/display-tables/fixed-asset-nbv/fixed-asset-nbv.module.ts
+++ src/main/webapp/app/bespoke/display-tables/fixed-asset-nbv/fixed-asset-nbv.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { FixedAssetNbvComponent } from './fixed-asset-nbv.component';
+import { DataTablesModule as DtTablesModule } from 'angular-datatables';
 
 const routes: Routes = [{ path: '', component: FixedAssetNbvComponent }];
 
 @NgModule({
   declarations: [FixedAssetNbvComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), DtTablesModule],
+  exports: [FixedAssetNbvComponent],
 })
 export class FixedAssetNbvModule {}
