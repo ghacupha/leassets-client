4c994068d0cb973501aa1bad63620eb5222268be
Edwin Njeru <mailnjeru@gmail.com>
2021-05-19 14:08:14 +0300

    Implement fixed-asset-nbv-summary.service.spec.ts test

--- src/main/webapp/app/bespoke/display-tables/fixed-asset-nbv/fixed-asset-nbv-summary.service.spec.ts
+++ src/main/webapp/app/bespoke/display-tables/fixed-asset-nbv/fixed-asset-nbv-summary.service.spec.ts
@@ -12,11 +12,41 @@ class NBVSummary {
  * here on the browser
  */
 describe('TDDNBVSummaryService', () => {
-  const nbv_entries: NBVSummary[] = [];
-  const expected_nbv_entries: NBVSummary[] = [];
+  const nbv_entries: NBVSummary[] = [
+    { serviceOutlet: 'AAA', category: 'CC', netBookValue: 1000 },
+    { serviceOutlet: 'AAA', category: 'CC', netBookValue: 1000 },
+    { serviceOutlet: 'AAA', category: 'CC', netBookValue: 1000 },
+    { serviceOutlet: 'AAA', category: 'dd', netBookValue: 3000 },
+    { serviceOutlet: 'AAA', category: 'dd', netBookValue: 3000 },
+    { serviceOutlet: 'AAA', category: 'dd', netBookValue: 3000 },
+    { serviceOutlet: 'AAA', category: 'dd', netBookValue: 3000 },
+    { serviceOutlet: 'BBB', category: 'CC', netBookValue: 2000 },
+    { serviceOutlet: 'BBB', category: 'CC', netBookValue: 2000 },
+    { serviceOutlet: 'BBB', category: 'CC', netBookValue: 2000 },
+    { serviceOutlet: 'BBB', category: 'CC', netBookValue: 2000 },
+    { serviceOutlet: 'AAA', category: 'ee', netBookValue: 4000 },
+    { serviceOutlet: 'AAA', category: 'ee', netBookValue: 4000 },
+    { serviceOutlet: 'AAA', category: 'ee', netBookValue: 4000 },
+  ];
+  const expected_nbv_entries: NBVSummary[] = [
+    { serviceOutlet: 'AAA', category: 'CC', netBookValue: 3000 },
+    { serviceOutlet: 'AAA', category: 'dd', netBookValue: 12000 },
+    { serviceOutlet: 'BBB', category: 'CC', netBookValue: 8000 },
+    { serviceOutlet: 'AAA', category: 'ee', netBookValue: 12000 },
+  ];
 
   it('should Calculate Summary of the NBV entries', function () {
-    const nbv_summary_groups: NBVSummary[] = [];
+    const nbv_summary_groups = lodash.groupBy(nbv_entries, entry => {
+      return [entry['serviceOutlet'], entry['category']];
+    });
+
+    const nbv_summary: NBVSummary[] = lodash.map(nbv_summary_groups, (objs, key) => {
+      return {
+        serviceOutlet: key.split(',')[0],
+        category: key.split(',')[1],
+        netBookValue: lodash.sumBy(objs, 'netBookValue'),
+      };
+    });
 
     expect(nbv_summary).toEqual(expected_nbv_entries);
   });
